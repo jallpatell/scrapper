52e8dd5b3ae6f5ebd84ed66c6c5d9d72fedf4005
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-06-07 02:01:03 +0530

    update url list

---
+++ list.js
@@ -0,0 +1,54 @@
+const companies = [
+    {
+        symbol: 'RELIANCE',
+        name: 'Reliance Industries Limited',
+        url: 'https://www.moneycontrol.com/india/stockpricequote/refineries/relianceindustries/RI'
+    },
+    {
+        symbol: 'TCS',
+        name: 'Tata Consultancy Services Limited',
+        url: 'https://www.moneycontrol.com/india/stockpricequote/computers-software/tataconsultancyservices/TCS'
+    },
+    {
+        symbol: 'INFY',
+        name: 'Infosys Limited',
+        url: 'https://www.moneycontrol.com/india/stockpricequote/computers-software/infosys/IT'
+    },
+    {
+        symbol: 'HDFCBANK',
+        name: 'HDFC Bank Limited',
+        url: 'https://www.moneycontrol.com/india/stockpricequote/banks-private-sector/hdfcbank/HDF01'
+    },
+    {
+        symbol: 'ICICIBANK',
+        name: 'ICICI Bank Limited',
+        url: 'https://www.moneycontrol.com/india/stockpricequote/banks-private-sector/icicibank/ICI2'
+    },
+    {
+        symbol: 'HINDUNILVR',
+        name: 'Hindustan Unilever Limited',
+        url: 'https://www.moneycontrol.com/india/stockpricequote/personal-care/hindustanunilever/HU'
+    },
+    {
+        symbol: 'SBIN',
+        name: 'State Bank of India',
+        url: 'https://www.moneycontrol.com/india/stockpricequote/banks-public-sector/statebankofIndia/SBI'
+    },
+    {
+        symbol: 'KOTAKBANK',
+        name: 'Kotak Mahindra Bank Limited',
+        url: 'https://www.moneycontrol.com/india/stockpricequote/banks-private-sector/kotakmahindrabank/KMB'
+    },
+    {
+        symbol: 'ITC',
+        name: 'ITC Limited',
+        url: 'https://www.moneycontrol.com/india/stockpricequote/diversified/itc/ITC'
+    },
+    {
+        symbol: 'LT',
+        name: 'Larsen & Toubro Limited',
+        url: 'https://www.moneycontrol.com/india/stockpricequote/infrastructure-general/larsentoubro/LT'
+    }
+];
+
+module.exports = { companies }
